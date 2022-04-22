f4c3560b33bf38645640c97e97a68ab1def61890
meayasohn <meaya.sohn@gmail.com>
2022-04-22 16:08:48 -0700

    Update first init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,124 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header reveal elevated class="bg-accent text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          <q-avatar>
+            <img
+              src="https://drive.google.com/file/d/1YmIN6kpmyjm2DWy44EfPuI-U6ZST9HgZ/view?usp=sharing"
+            />
+          </q-avatar>
+          ProviderSharing
+        </q-toolbar-title>
+
+        <div>v{{ version }}</div>
+      </q-toolbar>
+      <q-tabs align="left">
+        <!--중요한것 몇개만 link -->
+        <q-route-tab to="/About" label="About" />
+        <q-route-tab to="/program" label="Programs" />
+        <q-route-tab to="/contact" label="Contact" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Main menus </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksList = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {
+    EssentialLink,
+  },
+  data() {
+    return {
+      version: "01.00.00",
+    };
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="q-pa-md">
+    <q-carousel
+      v-model="slide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      animated
+      control-color="primary"
+      class="rounded-borders"
+    >
+      <q-carousel-slide name="style" class="column no-wrap flex-center">
+        <q-icon name="style" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="tv" class="column no-wrap flex-center">
+        <q-icon name="live_tv" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="layers" class="column no-wrap flex-center">
+        <q-icon name="layers" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+      <q-carousel-slide name="map" class="column no-wrap flex-center">
+        <q-icon name="terrain" color="primary" size="56px" />
+        <div class="q-mt-md text-center">
+          {{ lorem }}
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
+
+    <div class="row justify-center">
+      <q-btn-toggle
+        glossy
+        v-model="slide"
+        :options="[
+          { label: 1, value: 'style' },
+          { label: 2, value: 'tv' },
+          { label: 3, value: 'layers' },
+          { label: 4, value: 'map' },
+        ]"
+      />
+    </div>
+  </div>
+
+  <q-card
+    class="my-card text-white q-ma-sm"
+    style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)"
+  >
+    <q-card-section>
+      <div class="text-h6">공부하는 한의원</div>
+      <div class="text-subtitle2">by Body Identity</div>
+    </q-card-section>
+    <q-card-section class="q-pt-none">
+      <q-img
+        :src="`https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb`"
+        spinner-color="white"
+        class="rounded-borders"
+        style="width: 100%"
+      />
+      <div class="absolute-bottom text-subtitle1 text-center q-mb-lg">
+        {{ intro }}
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+  setup() {
+    return {
+      slide: ref("style"),
+      lorem:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque voluptatem totam, architecto cupiditate officia rerum, error dignissimos praesentium libero ab nemo provident incidunt ducimus iusto perferendis porro earum. Totam, numquam?",
+      intro:
+        "몸을 수리하러 한의원에 오지 마세요. Body Identity는 왜 아프게 되었는지 공부하는 곳입니다.",
+    };
+  },
+});
+</script>
